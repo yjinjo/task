6093e03b1cdd697f3b9e2149e2e98c495243854b
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-30 13:16:15 +0900

    #1. ListView를 통해 현재 시간 기준으로 Tasks 필터링

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.utils import timezone
+from django.views.generic import TemplateView, CreateView, ListView
 
 from taskapp.models import Task
 
@@ -13,7 +14,7 @@ class TaskListView(TemplateView):
     template_name = "pages/task_list.html"
 
     def get_context_data(self, **kwargs):
-        tasks = Task.objects.all()
+        tasks = Task.objects.filter(due__gte=timezone.now()).order_by("-due")
 
         return {"tasks": tasks}
 
@@ -23,3 +24,11 @@ class TaskCreateView(CreateView):
     fields = ["title", "type", "due"]
     template_name = "pages/task_create.html"
     success_url = "/"
+
+
+class TaskPreviousListView(ListView):
+    """Due Date 이 지난 Task 들을 보여주는 클래스"""
+
+    model = Task
+    template_name = "pages/task_previous_list.html"
+    queryset = Task.objects.filter(due__lt=timezone.now()).order_by("-due")
